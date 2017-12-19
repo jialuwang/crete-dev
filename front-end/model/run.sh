d43d3be62c9cadac15b62bf67db94d0a09f9caa2
jialuwang <jialuemail@gmail.com>
2017-12-19 14:05:40 -0800

    fix paths in e1000.c and modified run.sh for crete run

--- front-end/model/run.sh
+++ front-end/model/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #~/Projects/qklee/qemu-2.1.2/x86_64-softmmu/qemu-system-x86_64 -k us -enable-kvm -m 2048 -net nic,model=e1000 -net user -redir tcp:2022::22 -nographic ~/images/ubuntu-12.04.2-desktop-amd64.img
 #../qemu-2.1.2/x86_64-softmmu/qemu-system-x86_64 -k us -enable-kvm -m 2048 -net nic,model=e1000 -net user -redir tcp:2022::22 -usbdevice serial::stdio -serial stdio ~/qfm/ubuntu-12.04.2-desktop-amd64.img
-rm ../trace_parser/pci_dma_data
+rm -f ../trace_parser/pci_dma_data
 #../qemu-2.1.2/x86_64-softmmu/qemu-system-x86_64 -k us -m 2048 -net nic,model=e1000 -net user -redir tcp:2022::22 -usbdevice serial::stdio -serial stdio ~/qfm/ubuntu-12.04.2-desktop-amd64.img
 #../qemu-2.1.2/x86_64-softmmu/qemu-system-x86_64 -k us -m 2048 -net nic,model=e1000 -net user -redir tcp:2022::22 -usbdevice serial::stdio -serial stdio ../img/crete.img
 
-/home/jialuwang/crete/crete-dev/front-end/qemu-2.3/x86_64-softmmu/crete-qemu-2.3-system-x86_64 -k us -m 2048 -net nic,model=e1000  -serial stdio ~/qklee/img/crete.img
+#/home/jialuwang/crete/crete-dev/front-end/qemu-2.3/x86_64-softmmu/crete-qemu-2.3-system-x86_64 -k us -m 2048 -net nic,model=e1000 -enable-kvm -serial stdio ~/qklee/img/crete.img
 
-
+/home/jialuwang/crete/crete-dev/front-end/qemu-2.3/x86_64-softmmu/crete-qemu-2.3-system-x86_64 -k us -m 1024 -net nic,model=e1000 -net user -redir tcp:2022::22 -loadvm test -serial stdio ~/qklee/img/ubuntu-14.04-4.10.img -monitor telnet:127.0.0.1:1234,server,nowait
